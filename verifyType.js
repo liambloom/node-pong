1bae46c4c6c15f6d1d24aad88ee39d0ab872317d
liambloom <liamrbloom@gmail.com>
2020-03-20 01:37:44 -0400

    Better type verification

--- verifyType.js
+++ verifyType.js
@@ -1,42 +1,61 @@
+const color = require("./colorConsole");
+
 const getConstructor = value => value === null ? "null" : value === undefined ? "undefined" : value.constructor.name;
-function verifyConfig(config, propertyName, fallback, required = false) {
+function verifyConfig(config, propertyName, fallback, required = false, minValue, maxValue) {
   if (!config.hasOwnProperty(propertyName)) {
     if (required) throw new Error(`config.${propertyName} is required`);
     else return fallback;
   }
-  else return verify(config[propertyName], fallback, `config.${propertyName}`, required);
+  else return verify(config[propertyName], fallback, `config.${propertyName}`, required, minValue, maxValue);
 }
-function verify(value, fallback, valueName = "<value>", required = true) {
+function verify(value, fallback, valueName = "<value>", required = true, minValue = -Infinity, maxValue = Infinity) {
   if (required && fallback instanceof constructor) {
     try {
-      fallback = new fallback();
+      fallback = fallback();
     }
     catch (err) {
-      fallback = fallback();
+      fallback = new fallback();
     }
   }
   const useFallback = err => {
-    if (required) throw err;
-    else {
-      console.warn(`\x1b[33m${valueName} should be type ${getConstructor(fallback)}, but type ${getConstructor(value)} was recieved. ${valueName} defaulted to ${fallback}\x1b[0m`);
-      return fallback;
-    }
+    throwIfRequired(err);
+    return fallback;
+  };
+  const throwIfRequired = err => {
+    if (required) throw new Error(err);
+    else console.warn(err);
   };
   if (value == null || fallback == null) { // if either is null or undefined
     if (value === fallback) return value;
     else return useFallback(new Error(`${valueName} must be of type ${getConstructor(fallback)}, received type ${getConstructor(value)}`));
   }
-  else if (value.constructor === Number && fallback.constructor === Number) {
-    if (isNaN(value) === isNaN(fallback)) return value;
+  else if (typeof value === "object") {
+    if (typeof fallback === "object") {
+      if (value instanceof fallback.constructor) return value;
+      else return useFallback(new Error(`${valueName} must be of type ${fallback.constructor.name}, received type ${value.constructor.name}`));
+    }
+    else if (value.constructor === fallback.constructor) value = fallback.constructor(value);
+  }
+  if (typeof value === "number" && typeof fallback === "number") {
+    if (isNaN(value) && isNaN(fallback)) return value;
     else if (isNaN(value)) return useFallback(new Error(`${valueName} is NaN`));
     else if (isNaN(fallback)) return useFallback(new Error(`${valueName} must be NaN`));
+    else {
+      if (value <= minValue) {
+        throwIfRequired(`${valueName} ${required ? "must" : "should"} be at least ${minValue}`);
+        if (fallback <= minValue) return undefined;
+        else return fallback;
+      }
+      else if (value >= maxValue) {
+        throwIfRequired(`${valueName} ${required ? "must" : "should"} be at most ${maxValue}`);
+        if (fallback >= maxValue) return undefined;
+        else return fallback;
+      }
+      else return value;
+    }
   }
-  else if (value instanceof Object && fallback instanceof Object) {
-    if (value instanceof fallback.constructor) return value;
-    else return useFallback(new Error(`${valueName} must be of type ${fallback.constructor.name}, received type ${value.constructor.name}`));
-  }
-  else if (value.constructor === fallback.constructor) return value;
-  else return useFallback(new Error(`${valueName} must be of type ${fallback.constructor.name}, received type ${value.constructor.name}`));
+  else if (typeof value === typeof fallback) return value;
+  else return useFallback(new Error(`${valueName} must be of type ${typeof fallback}, received type ${typeof value}`));
 }
 verify.config = verifyConfig;
 module.exports = verify;
